4309e21bfe8b22faf72fdc377ff0ba157d0f7c50
Renan Pollo Benelli <Renansk18@outlook.com>
2023-08-22 17:03:27 -0300

    Atualização

---
+++ Models/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace WEBMVC.Models
+{
+    [Table("Alunos")]
+    public class Aluno
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(35)]
+        public string nome { get; set; }
+
+        public DateTime aniversario { get; set; }
+
+        public Curso curso { get; set; }
+
+        public string periodo { get; set; }
+    }
+}
---
+++ Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WEBMVC.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options): base(options) {}
+
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Sala> Salas { get; set; }
+        public DbSet<Atendimento> Atendimentos { get; set; }
+    }
+}
